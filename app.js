49dc1c1f23f044abfc58ff385e4bfc49d2123509
Prerna Choudhary <choudharyprerna124@gmail.com>
2023-01-06 10:05:09 +0530

    update

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://postman-echo.com/get"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
 
 function clickHandler(){
-    outputDiv.innerText = "ajsjsjsjsjs " + txtInput.value;
+    // outputDiv.innerText = "ajsjsjsjsjs " + txtInput.value;
+    var inputText = txtInput.ariaValueMax;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json))
 };
 
 btnTranslate.addEventListener("click", clickHandler)
